151785a5604c12f111fe07559f8648ca41e03870
Shubham Barve <shubham@barve.net>
2026-06-29 16:10:37 +0530

    made changes to create_engine

--- backend/database.py
+++ backend/database.py
@@ -14,7 +14,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=300,)
 
 SessionLocal = sessionmaker(
     autocommit=False,
